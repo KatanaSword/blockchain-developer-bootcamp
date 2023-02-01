eacaf7a899ef57452a83315f6860ed48d853e59e
saurabh <saurabh@LAPTOP-C9E80AGT.localdomain>
2023-02-01 12:57:01 +0530

    4. Token Balances

--- test/Token.js
+++ test/Token.js
@@ -6,12 +6,15 @@ const tokens = (n) => {
 }
 
 describe('Token', () => {
-    let token
+    let token, accounts, deployer
 
     beforeEach(async () => {
         // Fetch Token from Blockchain
         const Token = await ethers.getContractFactory('Token')
         token = await Token.deploy('Saurabh', 'SAH', '1000000')
+
+        accounts = await ethers.getSigners()
+        deployer = accounts[0]
     })
     
 describe('Deployment', () => {
@@ -34,6 +37,10 @@ describe('Deployment', () => {
 
     it('has correct total supply', async () => {
         expect(await token.totalSupply()).to.equal(totalSupply)
+    })
+    
+    it('assings total supply to deployer', async () => {
+        expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
     })
 
 // Describe spending...
