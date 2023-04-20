ddf849d6e44684831f740c8dd443b144fae0dc34
Saurabh Tajane <saurabh@LAPTOP-C9E80AGT>
2023-04-20 08:59:25 +0530

    mumbai deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,9 +7,12 @@ module.exports = {
   solidity: "0.8.9",
   networks: {
     localhost: {},
-    sepolia: {
-      url: `
-      https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY1}`,
+      accounts: privateKeys.split(','),
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY2}`,
       accounts: privateKeys.split(','),
     }
   },
